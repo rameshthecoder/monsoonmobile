a9acd1a03cb7260b5dd602f59d7ef127a84afb00
Ramesh <rvinu6@gmail.com>
2017-08-23 01:14:12 +0530

    Added VideoListActivity

---
+++ app/src/main/java/in/monsoonmedia/monsoonmobile/PlaylistAdapter.java
@@ -0,0 +1,10 @@
+package in.monsoonmedia.monsoonmobile;
+
+import android.widget.ArrayAdapter;
+
+/**
+ * Created by ramesh on 22/8/17.
+ */
+
+public class PlaylistAdapter extends ArrayAdapter {
+}
